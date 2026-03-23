89f900891fed070b574f398aba9ac85f8acc21f0
SimonovBlonsky <995160396@qq.com>
2026-03-23 23:20:45 +0800

    test: tighten loop descriptor coverage

--- tests/model/test_loop_descriptor.py
+++ tests/model/test_loop_descriptor.py
@@ -6,12 +6,26 @@ from depth_anything_3.model.loop_descriptor import (
 )
 
 
-def test_build_loop_descriptor_l2_normalizes_concat_descriptor():
+def test_build_loop_descriptor_matches_expected_concat_content():
     camera = torch.tensor([[3.0, 4.0]])
     patches = torch.tensor([[[1.0, 0.0], [0.0, 1.0]]])
+
     descriptor = build_loop_descriptor(camera, patches)
+
+    expected = torch.tensor([[0.42426407, 0.56568545, 0.5, 0.5]])
     assert descriptor.shape == (1, 4)
-    assert torch.allclose(torch.linalg.norm(descriptor, dim=-1), torch.ones(1), atol=1e-6)
+    assert torch.allclose(descriptor, expected, atol=1e-6)
+
+
+def test_build_loop_descriptor_uses_gem_pooling_when_requested():
+    camera = torch.tensor([[3.0, 4.0]])
+    patches = torch.tensor([[[1.0, 0.0], [0.0, 1.0]]])
+    weights = torch.tensor([[0.0, 1.0]])
+
+    descriptor = build_loop_descriptor(camera, patches, weights=weights, pooling="gem")
+
+    expected = torch.tensor([[0.42426407, 0.56568545, 0.0, 0.70710677]])
+    assert torch.allclose(descriptor, expected, atol=1e-6)
 
 
 def test_confidence_weighted_pool_suppresses_low_conf_tokens():
